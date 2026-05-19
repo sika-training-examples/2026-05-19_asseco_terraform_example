35df2f20db7bca83bb80ee51f214f7a14a11c40c
Ondrej Sika <ondrej@ondrejsika.com>
2026-05-19 11:04:09 +0000

    feat: add networks

---
+++ net1.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet-${local.prefix}-net1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
---
+++ net2.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "vnet-${local.prefix}-net2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.2.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = ["10.2.1.0/24"]
+}
